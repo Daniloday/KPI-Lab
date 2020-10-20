fe7653c669392303a199c2b6f63c993a9086299b
daniloday <danil.vladyka1@gmail.com>
2020-10-20 20:55:17 +0300

    multi

--- functions/Multimodal.py
+++ functions/Multimodal.py
@@ -2,3 +2,9 @@ import numpy as np
 
 def f(x, y):
     return x * np.sin(4 * np.pi * x) - y * np.sin(4 * np.pi * y + np.pi) + 1
+
+def dfdx(x, y):
+	return np.sin(4 * np.pi * x) + x * np.cos(4 * np.pi * x) * 4 * np.pi
+
+def dfdy(x, y):
+	return - np.sin(4 * np.pi * y + np.pi) - y * np.cos(4 * np.pi * y + np.pi) * 4 * np.pi
--- functions/Quadratic.py
+++ functions/Quadratic.py
@@ -2,3 +2,6 @@ import numpy as np
 
 def f(x, y):
     return 10 * (x ** 2 - y) ** 2 + (x - 1)**2
+
+def dfdx(x, y):
+	
