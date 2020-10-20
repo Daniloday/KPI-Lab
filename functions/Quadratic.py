9dece06e33961303063c21bab570ec36b0b22e37
Alex Halitsa <alex.dragonfox@gmail.com>
2020-10-20 21:21:06 +0300

    fix

--- functions/Quadratic.py
+++ functions/Quadratic.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 def f(x, y):
-    return 10 * (x ** 2 - y) ** 2 + (x - 1)**2
+    return 10 * (x ** 2 - y) ** 2 + (x - 1) ** 2
 
 def dfdx(x, y):
-	
+    return 2 * (20 * x ** 3 - 20 * x * y + x - 1)
+
+def dfdy(x, y):
+    return -20 * (x ** 2 - y)
